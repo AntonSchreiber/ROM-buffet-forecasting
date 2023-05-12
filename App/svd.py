34e200a8ec73395f04489ea05254f138aacca25f
AntonSchreiber <anton.schreiber@tu-braunschweig.de>
2023-05-12 16:33:26 +0200

    Create SVD of X and plot contributions

--- App/svd.py
+++ App/svd.py
@@ -1,8 +1,11 @@
 from modules.preprocessing import load_data
 import torch as pt
 from torch import flatten
-import flowtorch as ft
+from flowtorch.analysis import SVD
 import random
+import bisect
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 random.seed(711)
 
 if __name__ == "__main__":
@@ -28,5 +31,32 @@ if __name__ == "__main__":
     for i in range(1, len(train_keys)):
         X_train = pt.concat((X_train, cp_084_data[train_keys[i]].flatten(0, 1)), dim=0)
     print("Shape of train_data is:  ", X_train.shape)
+
+    # Apply Singular Value Decomposition
+    rank = 58
+    svd = SVD(X_train, rank=rank)
+    print(svd)
+
+    s = svd.s
+    s_sum = s.sum().item()
+    # relative contribution
+    s_rel = [s_i / s_sum * 100 for s_i in s]
+    # cumulative contribution
+    s_cum = [s[:n].sum().item() / s_sum * 100 for n in range(s.shape[0])]
+    # find out how many singular values we need to reach at least 90 percent
+    i_90 = bisect.bisect_right(s_cum, 90)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    ax1.bar(range(s.shape[0]), s_rel, align="edge")
+    ax2.plot(range(s.shape[0]), s_cum)
+    ax2.set_xlim(0, rank)
+    ax2.set_ylim(0, 105)
+    ax1.set_title("individual contribution in %")
+    ax2.set_title("cumulative contribution in %")
+    ax2.plot([0, i_90, i_90], [s_cum[i_90], s_cum[i_90], 0], ls="--", color="C3")
+    ax2.text(i_90+1, 45, "first {:d} singular values yield {:1.2f}%".format(i_90, s_cum[i_90]))
+    plt.show()
+
+
     
 
